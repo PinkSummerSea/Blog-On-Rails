e6cd6d505a78c71815aa380cef68655c1e7bdc16
Xiaojun Lin <xiaojunlin@XiaojundeMacBook-Pro.local>
2022-07-16 13:12:51 -0700

    bootstrop installed. index page responsive design finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
+
+  root "posts#index"
 end
